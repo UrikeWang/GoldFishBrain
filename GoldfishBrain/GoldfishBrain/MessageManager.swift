9b355445bc7b7280ea34b83cdfcf1193439452ec
yuling <takusax@gmail.com>
2017-08-01 14:47:57 +0800

    show chat content in chatroom

--- GoldfishBrain/GoldfishBrain/MessageManager.swift
+++ GoldfishBrain/GoldfishBrain/MessageManager.swift
@@ -38,73 +38,111 @@ class MessageManager {
 
         }
 
-        let ref = Database.database().reference().child("user-messages").child(uid)
+        let ref = Database.database().reference().child("users").child(uid).child("chats")
+
+        let channelRef = Database.database().reference().child("channels")
 
         ref.observe(.value, with: { (snapshot) in
 
-            let messageID = snapshot.key
+            switch snapshot.childrenCount {
 
-            let messagesRef = Database.database().reference().child("messages")
+            case 0 :
 
-            //swiftlint:disable force_cast
-            let uid =  Auth.auth().currentUser?.uid as! String
-            //swiftlint:enable force_cast
+                print("no chat contents!")
 
-            messagesRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            case _ where snapshot.childrenCount > 0 :
 
-                for message in (snapshot.value as? [String: AnyObject])! {
+                for chat in (snapshot.value as? [String: Any])! {
 
-//                    print("snapshot.value", message)
+                    //channels ID
+                    if let chatroomID = chat.key as? String {
 
-                    if let dicts = message.value as? [String: Any] {
+                        channelRef.observeSingleEvent(of: .value, with: { (snapshot) in
 
-                        if let text = dicts["text"] as? String, let fromID = dicts["fromID"] as? String, let toID = dicts["toID"] as? String, let timestamp = dicts["timestamp"] as? Int {
+                            for channel in (snapshot.value as? [String: Any])! {
 
-                            if fromID == uid || toID == uid {
+                                if chatroomID == channel.key {
 
-                                if fromID == uid {
+                                    channelRef.child(chatroomID).observe(.value, with: { (snapshot) in
 
-                                    self.talk = Message(text: text, fromID: fromID, toID: toID, timestamp: timestamp)
+                                        for message in (snapshot.value as? [String: Any])! {
+
+                                            if let dicts = message.value as? [String: Any] {
+
+                                                if let text = dicts["text"] as? String, let fromID = dicts["fromID"] as? String, let toID = dicts["toID"] as? String, let timestamp = dicts["timestamp"] as? Int {
+
+                                                    if fromID == uid || toID == uid {
+
+                                                        if fromID == uid {
+
+                                                            self.talk = Message(text: text, fromID: fromID, toID: toID, timestamp: timestamp)
+
+                                                        }
+
+                                                        if toID == uid {
+
+                                                            self.talk = Message(text: text, fromID: toID, toID: fromID, timestamp: timestamp)
+
+                                                        }
+
+                                                        self.messagesdictionary[toID] = self.talk
+
+                                                        self.messages = Array(self.messagesdictionary.values)
+
+                                                        self.messages.sort(by: { (talk1, talk2) -> Bool in
+
+                                                            // TODO: 修正最後訊息顯示
+                                                            //                                    print("talk1", talk1.timestamp, talk1.text)
+                                                            //
+                                                            //                                    print("talk2", talk2.timestamp, talk2.text)
+
+                                                            return talk1.timestamp > talk2.timestamp
+
+                                                        })
+
+                                                        self.delegate?.messageManager(self, didGetMessage: self.messages)
+                                                        //
+                                                        //                                }
+
+                                                    }
+
+                                                } else {
+
+                                                    print("Message data fetch failed")
+
+                                                }
+
+                                            }
+                                        }
+
+                                    }, withCancel: nil)
+
+                                    if let channelContent = channel.value as? [String: Any] {
+
+                                        if let member = channelContent["members"] as? [AnyObject] {
+
+                                            //swiftlint:disable force_cast
+                                            let chatMember1 = member[0] as! String
+
+                                            let chatMember2 = member[1] as! String
+                                            //swiftlint:enable force_cast
+
+                                        }
+
+                                    }
 
                                 }
-
-                                if toID == uid {
-
-                                    self.talk = Message(text: text, fromID: toID, toID: fromID, timestamp: timestamp)
-
-                                }
-
-                                    self.messagesdictionary[toID] = self.talk
-
-                                    self.messages = Array(self.messagesdictionary.values)
-
-                                    self.messages.sort(by: { (talk1, talk2) -> Bool in
-
-                                        // TODO: 修正最後訊息顯示
-                                        //                                    print("talk1", talk1.timestamp, talk1.text)
-                                        //
-                                        //                                    print("talk2", talk2.timestamp, talk2.text)
-
-                                        return talk1.timestamp > talk2.timestamp
-
-                                    })
-
-                                self.delegate?.messageManager(self, didGetMessage: self.messages)
-//
-//                                }
-
                             }
 
-                        } else {
-
-                            print("Message data fetch failed")
-
-                        }
+                        }, withCancel: nil)
 
                     }
+
                 }
 
-            }, withCancel: nil)
+            default : break
+
+            }
 
         }, withCancel: nil)
 
